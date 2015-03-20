32ef17fff87cbfdacf5d6e8f45224a22eee3b557
Vilkazz <baka367@gmail.com>
2015-03-20 21:39:51 +0200

    Dump of the entire table data instead of just 1 call. WARNING... SLOW!!!

--- src/APIBundle/Controller/DefaultController.php
+++ src/APIBundle/Controller/DefaultController.php
@@ -5,42 +5,69 @@ namespace APIBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use XMLWriter;
 use GuzzleHttp;
+use GuzzleHttp\Exception\ConnectException;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-//        $filename = $this->get('kernel')->getRootDir().'/uploads/json_data.json';
-//        $filename = "json_data.json";
+        $client = new GuzzleHttp\Client();
+        try {
+            $res = $client->get('http://wonderwall.ox.nfq.lt/kickertable/api/v1/events?rows=100&from-id=20', ['auth' => ['nfq', 'labas']]);
+        } catch(ConnectException $e){
+            return $this->render('APIBundle:Default:index.html.twig', array('route' => 'Connection error'));
+        }
 //        $handle = fopen($filename, "r");
 //        $contents = fread($handle, filesize($filename));
-//        $converted_array = json_decode($contents);
-        $client = new GuzzleHttp\Client();
-        $res = $client->get('http://wonderwall.ox.nfq.lt/kickertable/api/v1/events?rows=100&from-id=20', ['auth' =>  ['nfq', 'labas']]);
 
         $convertedArray = $res->json();
-//        print_r($convertedArray['records']);
-//        exit;
+
         $xmlWriter = new XMLWriter();
         $xmlWriter->openURI($this->get('kernel')->getRootDir().'/uploads/xml_writer_output.xml');
         $xmlWriter->startDocument('1.0','UTF-8');
         $xmlWriter->setIndent(4);
         $xmlWriter->startElement('results');
-        foreach($convertedArray['records'] as $record)
-        {
-            $xmlWriter->startElement('entry');
-            $xmlWriter->writeElement('id', $record['id']);
-            $xmlWriter->writeElement('timeSec', date('Y-m-d, H:m:s',$record['timeSec']));
-            $xmlWriter->writeElement('usec', $record['usec']);
-            $xmlWriter->writeElement('type', $record['type']);
-            $xmlWriter->writeElement('data', $record['data']);
-            $xmlWriter->endElement();
+
+        if (is_array($convertedArray['records'])) {
+            foreach ($convertedArray['records'] as $record) {
+                $xmlWriter->startElement('entry');
+                $xmlWriter->writeElement('id', $record['id']);
+                $xmlWriter->writeElement('timeSec', date('Y-m-d, H:m:s', $record['timeSec']));
+                $xmlWriter->writeElement('usec', $record['usec']);
+                $xmlWriter->writeElement('type', $record['type']);
+                $xmlWriter->writeElement('data', $record['data']);
+                $xmlWriter->endElement();
+            }
         }
-        $xmlWriter->endElement();
-        $xmlWriter->endDocument();
+        else return $this->render('APIBundle:Default:index.html.twig', array('route' => 'Illegal API result'));
 
-        $xmlWriter->flush();
+        while(sizeof($convertedArray['records']) == 100) {
+//        for($i = 0; $i < 10; $i++){
+            try {
+                $res = $client->get('http://wonderwall.ox.nfq.lt/kickertable/api/v1/events?rows=100&from-id=' . end($convertedArray['records'])['id'], ['auth' => ['nfq', 'labas']]);
+            } catch(ConnectException $e){
+                return $this->render('APIBundle:Default:index.html.twig', array('route' => 'Connection error'));
+            }
 
-        return $this->render('APIBundle:Default:index.html.twig', array('route' => $this->get('kernel')->getRootDir()));
+            $convertedArray = $res->json();
+
+            if (is_array($convertedArray['records'])) {
+                foreach ($convertedArray['records'] as $record) {
+                    $xmlWriter->startElement('entry');
+                    $xmlWriter->writeElement('id', $record['id']);
+                    $xmlWriter->writeElement('timeSec', date('Y-m-d, H:m:s', $record['timeSec']));
+                    $xmlWriter->writeElement('usec', $record['usec']);
+                    $xmlWriter->writeElement('type', $record['type']);
+                    $xmlWriter->writeElement('data', $record['data']);
+                    $xmlWriter->endElement();
+                }
+            }
+            else return $this->render('APIBundle:Default:index.html.twig', array('route' => 'Illegal API result'));
+        }
+            $xmlWriter->endElement();
+            $xmlWriter->endDocument();
+
+            $xmlWriter->flush();
+            return $this->render('APIBundle:Default:index.html.twig', array('route' => end($convertedArray['records'])['id']));
     }
 }
