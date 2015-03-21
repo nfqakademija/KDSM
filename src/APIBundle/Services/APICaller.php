f38b404f51cce9ec1ec76725fb7b783e581e00ed
Vilkazz <baka367@gmail.com>
2015-03-21 16:26:09 +0200

    URL and login credentials moved to services.yml. Start index set to 1 (0 retuns latest indexes, thus it cannot be used in this scope).

--- src/APIBundle/Services/APICaller.php
+++ src/APIBundle/Services/APICaller.php
@@ -6,10 +6,20 @@ use GuzzleHttp\Exception\ConnectException;
 
 class APICaller {
 
-    public function callApi($count = 100, $startId = 20){
+    protected $url;
+    protected $user;
+    protected $password;
+
+    public function __construct($url, $user, $password){
+        $this->url = $url[0];
+        $this->user = $user[0];
+        $this->password = $password[0];
+    }
+
+    public function callApi($count = 100, $startId = 1){
         $client = new GuzzleHttp\Client();
         try {
-            $res = $client->get('http://wonderwall.ox.nfq.lt/kickertable/api/v1/events?rows=' . $count . '&from-id=' . $startId, ['auth' => ['nfq', 'labas']]);
+            $res = $client->get($this->url . '?rows=' . $count . '&from-id=' . $startId, ['auth' => [$this->user, $this->password]]);
         } catch(ConnectException $e){
             return false;
         }
