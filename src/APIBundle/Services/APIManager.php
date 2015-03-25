25dcdf2d5fffef0f99d87a7fd218da0d15074149
Vilkazz <baka367@gmail.com>
2015-03-25 17:58:49 +0200

    Added missing file to commit

--- src/APIBundle/Services/APIManager.php
+++ src/APIBundle/Services/APIManager.php
@@ -7,25 +7,55 @@
  */
 
 namespace APIBundle\Services;
+use Symfony\Component\Form\Exception\BadMethodCallException;
 
 class APIManager {
 
     protected $caller;
     protected $writer;
 
+//    public function __construct(
+//
+//        $this->caller = new APICaller('','',''); //added for IDE to be able to recognice the methods....
+//    }
+
     public function setCaller(APICaller $caller){
         $this->caller = $caller;
     }
 
-    public function setParser(APIXmlWriter $writer){
-        $this->writer = $writer;
+    public function setParser($type = 'csv', $name = 'xml_writer_output', $path = 'app/uploads/'){
+//        if (!file_exists($path)) {
+//            mkdir($path, 0777, true);
+//        }
+        if ($type == 'xml')
+            $this->writer = new APIXmlWriter($path, $name . '.xml');
+        else if ($type == 'csv')
+            $this->writer = new APICsvWriter($path, $name . '.csv');
+        else throw new BadMethodCallException('Incorrect file type provided. Only [csv] and [xml] are accepted');
     }
 
-    public function getCaller(){
-        return $this->caller;
-    }
+    public function crawlAPI()
+    {
+        $writer= $this->writer;
+        $caller = $this->caller;
 
-    public function getParser(){
-        return $this->writer;
+        echo "writing document head... \r\n";
+        $writer->writeDocumentHead();
+
+        echo "connecting to api... \r\n";
+        $convertedArray = $this->caller->callApi();
+
+        $writer->writeArray($convertedArray);
+        echo 'writing api response id= ' . end($convertedArray['records'])['id'] . "\r\n";
+
+        while (sizeof($convertedArray['records']) == 100) {
+//        for($i = 0; $i < 10; $i++){
+
+            $convertedArray = $caller->callApi(100, end($convertedArray['records'])['id']);
+            $writer->writeArray($convertedArray);
+            echo 'writing api response id= ' . end($convertedArray['records'])['id'] . "\r\n";
+        }
+        echo "writing document footer... \r\n";
+        $writer->writeDocumentFooter();
     }
 }
