7074d2109254e059249cb8ec28c84029b7f94769
Vilkazz <baka367@gmail.com>
2015-03-26 17:00:24 +0200

    vis.js chart

--- src/APIBundle/Controller/DefaultController.php
+++ src/APIBundle/Controller/DefaultController.php
@@ -4,10 +4,15 @@ namespace APIBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use APIBundle\Services\ShakeContainer;
+
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('APIBundle:Default:index.html.twig', array('route' => $this->get('kernel')->getRootDir()));
+        
+        $shakeContainer = new ShakeContainer($this->get('kernel')->getRootDir()."/uploads/", 'failas');
+        $shakeContainer->setShakes();
+        return $this->render('APIBundle:Default:index.html.twig', array('route' => $shakeContainer->getShakes()));
     }
 }
