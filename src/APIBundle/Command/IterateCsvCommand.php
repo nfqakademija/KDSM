07cd515c711e48b40185fe7c50c3ae055129abba
Vilkazz <baka367@gmail.com>
2015-03-26 14:46:24 +0200

    CSV iterator file can now be selected

--- src/APIBundle/Command/IterateCsvCommand.php
+++ src/APIBundle/Command/IterateCsvCommand.php
@@ -8,6 +8,7 @@
 
 namespace APIBundle\Command;
 
+use APIBundle\Services\APICsvIterator;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -18,19 +19,30 @@ class IterateCsvCommand extends ContainerAwareCommand{
     protected function configure()
     {
         $this->setName('api:iterate')
+            ->addArgument('fileName', InputArgument::REQUIRED, 'csv file location')
             ->addArgument('count', InputArgument::OPTIONAL, 'Desired element count');
     }
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $iterator = $this->getContainer()->get('api.api_csv_iterator');
+        $path = 'app/uploads/';
+        $file = $input->getArgument('fileName');
+        $iterator = new APICsvIterator($path, $file . '.csv', ',', ['id', 'timesec', 'usec', 'type', 'data']);
+//        $iterator = $this->getContainer()->get('api.api_csv_iterator');
         if($input->getArgument('count') && is_numeric($input->getArgument('count'))){
             for($i = 0; $i < $input->getArgument('count'); $i++){
                 print_r($iterator->current());
             }
         }
-        else
+        else {
+//            $i = 0;
+//            foreach ($iterator as $key => $row) {
+//                $i += $i < count($row) - 1 ? 1 : -$i;
+//                echo "{$key}: {$i}: {$row[$i]}\n";
+//            }
+//        }
             while ($iterator->next())
                 print_r($iterator->current());
+        }
     }
 
 }
