f17acf15691e43c9e29745fe8ed22a6f5bad5baf
Vilkazz <baka367@gmail.com>
2015-03-31 15:05:15 +0300

    Added file, missing in last commit

--- src/APIBundle/Controller/DefaultController.php
+++ src/APIBundle/Controller/DefaultController.php
@@ -11,9 +11,10 @@ class DefaultController extends Controller
     public function indexAction()
     {
         
-        $shakeContainer = new ShakeContainer($this->get('kernel')->getRootDir()."/uploads/", 'failas', '2015-01-29');
+        $shakeContainer = new ShakeContainer($this->get('kernel')->getRootDir()."/uploads/", 'failas', '2015-03-19');
         $shakeContainer->setShakes();
         return $this->render('APIBundle:Default:index.html.twig', array('shakes' => $shakeContainer->getShakes(),
-            'goals' => $shakeContainer->getGoals(), 'date' => $shakeContainer->getDate()));
+            'goals' => $shakeContainer->getGoals(), 'swipes' => $shakeContainer->getSwipes(),
+            'date' => $shakeContainer->getDate()));
     }
 }
