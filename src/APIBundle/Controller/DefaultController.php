6c4f45559342821bfa32aa15ab8224e7a582bf87
Vilkazz <baka367@gmail.com>
2015-03-31 10:45:52 +0300

    Rewrote code to remove unnecessary second iteration and cleaned up for readability

--- src/APIBundle/Controller/DefaultController.php
+++ src/APIBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace APIBundle\Controller;
 
-use APIBundle\Services\GoalContainer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use APIBundle\Services\ShakeContainer;
@@ -14,9 +13,7 @@ class DefaultController extends Controller
         
         $shakeContainer = new ShakeContainer($this->get('kernel')->getRootDir()."/uploads/", 'failas');
         $shakeContainer->setShakes();
-        $goalContainer = new GoalContainer($this->get('kernel')->getRootDir()."/uploads/", 'failas');
-        $goalContainer->setGoals();
         return $this->render('APIBundle:Default:index.html.twig', array('shakes' => $shakeContainer->getShakes(),
-            'goals' => $goalContainer->getGoals()));
+            'goals' => $shakeContainer->getGoals()));
     }
 }
--- src/APIBundle/Services/ShakeContainer.php
+++ src/APIBundle/Services/ShakeContainer.php
@@ -9,54 +9,50 @@ namespace APIBundle\Services;
 
 class ShakeContainer {
     private $shakes;
+    private $goals;
+    private $refDate;
     private $iterator;
 
     public function __construct($path, $fileName){
-//        $path = 'app/uploads/';
         $this->iterator = new APICsvIterator($path, $fileName.'.csv', ',', ['id', 'timesec', 'usec', 'type', 'data']);
+        $this->refDate = date('Y-m-d', strtotime('2014-10-03'));
     }
 
     public function setShakes(){
         $object = $this->iterator->current();
 
-        $shakeKey = date('Y-m-d H:i:s', $object['timesec']);
+        $shakeValue = $goalValue = 0;
 
-        $refDate = date('Y-m-d', $object['timesec']);
-        $refDate = date('Y-m-d', strtotime('2014-12-04'));
-        $binValue = 0;
-
-        while($refDate != date('Y-m-d', $object['timesec'])) //go until target date
+        while($this->refDate != date('Y-m-d', $object['timesec'])) //go until target date
             $object = $this->iterator->current();
 
-        $refTimeStamp = date('H', $object['timesec']) . date('i', $object['timesec']); //get first timestamp
+        $refTimeStamp = $this->getObjectHrMin($object); //get first timestamp
 
-        while($refDate == date('Y-m-d', $object['timesec'])){   //while target date
+        while($this->refDate == date('Y-m-d', $object['timesec'])){   //while target date
 
-            if (date('H', $object['timesec']) . date('i', $object['timesec']) == $refTimeStamp){
+            if ($this->getObjectHrMin($object) == $refTimeStamp){
                 if ($object['type'] == 'TableShake')
-                    $binValue++;
+                    $shakeValue++;
+                if ($object['type'] == 'AutoGoal')
+                    $goalValue++;
 
-                $object = $this->iterator->current(); //get next object
+                $object = $this->getNextObject();
             }
             else {
-                if($refDate == date('Y-m-d', $object['timesec'])) {
-                    $this->shakes[date('Y-m-d H:i:s',strtotime($refDate.substr($refTimeStamp, 0, 2).':'.substr($refTimeStamp, -2).':00'))] = $binValue; //white timebin and event count
-                    $binValue = 0; //reset event count
+                if($this->refDate == date('Y-m-d', $object['timesec'])) {
+                    $this->writeBinValues($shakeValue, $goalValue, $refTimeStamp);
+                    $shakeValue = $goalValue = 0;
 
-                    $object = $this->iterator->current(); //get next object
+                    $object = $this->getNextObject();
 
                     $refTimeStamp = $this->getNextTimestamp($refTimeStamp);
-
-                    while($refTimeStamp != date('H', $object['timesec']) . date('i', $object['timesec']))
+                    while($refTimeStamp != $this->getObjectHrMin($object))
                     {
-                        $this->shakes[date('Y-m-d H:i:s',strtotime($refDate.substr($refTimeStamp, 0, 2).':'.substr($refTimeStamp, -2).':00'))] = 0;
+                        $this->writeBinValues(0, 0, $refTimeStamp);
                         $refTimeStamp = $this->getNextTimestamp($refTimeStamp);
-//                        echo $refTimeStamp . ' ' . date('H', $object['timesec']) . date('i', $object['timesec']);
-//                        exit;
                     }
                 }
             }
-//                $this->shakes[] = date('Y-m-d H:i:s', $object['timesec']);
         }
 
     }
@@ -65,20 +61,38 @@ class ShakeContainer {
         return $this->shakes;
     }
 
-    private function getNextTimestamp($currentTimestamp){
-        if($currentTimestamp == 2400)
+    public function getGoals(){
+        return $this->goals;
+    }
+
+    private function getNextObject(){
+        return $this->iterator->current();
+    }
+
+    private function getObjectHrMin($object){
+        return date('H', $object['timesec']) . date('i', $object['timesec']);
+    }
+
+    private function writeBinValues($shakes, $goals, $refTimeStamp){
+        $this->shakes[date('Y-m-d H:i:s',strtotime($this->refDate.substr($refTimeStamp, 0, 2).':'.substr($refTimeStamp, -2).':00'))] = $shakes; //white timebin and event count
+        $this->goals[date('Y-m-d H:i:s',strtotime($this->refDate.substr($refTimeStamp, 0, 2).':'.substr($refTimeStamp, -2).':00'))] = $goals; //white timebin and event count
+
+    }
+
+    private function getNextTimestamp($currentTimeStamp){
+        if($currentTimeStamp == 2400)
             return false;
-        if(substr($currentTimestamp, -2) < 60) {
+        if(substr($currentTimeStamp, -2) < 60) {
 //            echo $currentTimestamp;
-            $newMin = substr($currentTimestamp, -2);
+            $newMin = substr($currentTimeStamp, -2);
             $newMin++;
 
-            return ($newMin < 10) ? substr($currentTimestamp, 0, 2) . '0' . $newMin :
-                substr($currentTimestamp, 0, 2) . $newMin;
+            return ($newMin < 10) ? substr($currentTimeStamp, 0, 2) . '0' . $newMin :
+                substr($currentTimeStamp, 0, 2) . $newMin;
         }
-        if(substr($currentTimestamp, -2) == 60)
+        if(substr($currentTimeStamp, -2) == 60)
         {
-            $hr = substr($currentTimestamp, 0, 2);
+            $hr = substr($currentTimeStamp, 0, 2);
             $hr++;
             return $hr < 10 ? '0' . $hr . '00' : $hr . '00';
 
