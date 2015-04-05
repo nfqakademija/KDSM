2ec599004165a69ddc3b99e79d3b01bf3e7b637f
Dainius Šležas <dn.slezas@gmail.com>
2015-04-05 11:14:18 +0300

    Added new indexes

--- src/KDSM/ContentBundle/Controller/UsersController.php
+++ src/KDSM/ContentBundle/Controller/UsersController.php
@@ -1,19 +1,33 @@
 <?php
 
-namespace KDSM\APIBundle\Controller;
+namespace KDSM\ContentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UsersController extends Controller
 {
+
+    public function loginAction()
+    {
+        return $this->render('KDSMContentBundle:Users:login.html.twig', array('route' => $this->get('kernel')->getRootDir()));
+
+    }
+
+    public function registerAction()
+    {
+        return $this->render('KDSMContentBundle:Users:register.html.twig', array('route' => $this->get('kernel')->getRootDir()));
+
+    }
+
     public function indexAction()
     {
-        return $this->render('KDSMAPIBundle:Users:index.html.twig', array('route' => $this->get('kernel')->getRootDir()));
+        return $this->render('KDSMContentBundle:Users:index.html.twig', array('route' => $this->get('kernel')->getRootDir()));
     }
 
     public function viewAction()
     {
-        return $this->render('KDSMAPIBundle:Users:view.html.twig', array('route' => $this->get('kernel')->getRootDir()));
+        return $this->render('KDSMContentBundle:Users:view.html.twig', array('route' => $this->get('kernel')->getRootDir()));
 
     }
+
 }
