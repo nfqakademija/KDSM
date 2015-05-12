767e8335b93f5a40c2c9870a0d7d8d6b5da9dd4c
baka367 <baka367@gmail.com>
2015-05-12 23:42:09 +0300

    Fixed setting for live server

--- src/KDSM/ContentBundle/Services/QueueManager.php
+++ src/KDSM/ContentBundle/Services/QueueManager.php
@@ -287,7 +287,7 @@ class QueueManager
             $acceptedUserCountInQueue = $this->usersQueuesRepository->getAcceptedUserCount($queueObject);
             if ($acceptedUserCountInQueue < 3) {
                 $userQueueObject->setUserStatusInQueue('inviteAccepted');
-                if ($acceptedUserCountInQueue == 1) {
+                if ($acceptedUserCountInQueue >= 0) {
                     $queueObject->setStatus('in_queue');
                 }
                 $queueJoinResponse['response'] = 'Accept SUCCESS';
